1e1fc76f0172f4da8669a1bcc517058cb41d25cd
Pais Murama <paizinhomurama@gmail.com>
2022-11-04 15:50:41 +0200

    Primeiro Commit

---
+++ src/main/java/com/example/algamoney/api/util/GeradorSenha.java
@@ -0,0 +1,18 @@
+package com.example.algamoney.api.util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class GeradorSenha {
+	
+	public static void main(String[] args) throws Exception {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		System.out.println(encoder.encode("admin"));
+		
+	}
+
+	
+	
+	
+
+}
